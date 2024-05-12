037afab8e00ab3333723fe2cf9f373a40468bac5
Valentina-rapunzel <111701493+Valentina-rapunzel@users.noreply.github.com>
2024-05-12 16:17:59 +0200

    Added a default city

--- script.js
+++ script.js
@@ -30,8 +30,7 @@ currentDate.innerHTML = formatDate(todaysDate);
 // Show the temperature
 
 function displayTemperature(response) {
-  
-  // Injected an icon 
+  // Injected an icon
   let iconElement = document.querySelector("#current-temperature-icon");
   let icon = `<img src="${response.data.condition.icon_url}" class="current-temperature-icon" alt="current-temperature-icon"/>`;
   iconElement.innerHTML = `${icon}`;
@@ -39,6 +38,9 @@ function displayTemperature(response) {
   let temperatureElement = document.querySelector("#current-temperature-value");
   console.log(response);
   console.log(response.data);
+  let unitElement = document.querySelector("#current-temperature-unit");
+  let unit = `°C`;
+  unitElement.innerHTML = unit;
   let temperature = response.data.temperature.current;
   temperatureElement.innerHTML = Math.round(temperature);
 
@@ -64,20 +66,25 @@ function displayTemperature(response) {
   windElement.innerHTML = `${wind}km/h`;
 }
 
-// Show city
+// an API Call
 
-function search(event) {
-  event.preventDefault();
-
-  let searchInputElement = document.querySelector("#search-input");
-  let city = `${searchInputElement.value},`;
-
+function searchCity(city) {
   let key = "b30a2d9fef22b5o0t83182be74814ec8";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${key}&=metric`;
-
   axios.get(apiUrl).then(displayTemperature);
 }
 
+// Get the data from the input
+
+function showCity(event) {
+  event.preventDefault();
+  let searchInputElement = document.querySelector("#search-input");
+  searchCity(searchInputElement.value);
+}
+// show a dafault city
+
 let form = document.querySelector("#search-form");
 console.log("form");
-form.addEventListener("submit", search);
+form.addEventListener("submit", showCity, formatDate);
+
+searchCity("Zurich");
